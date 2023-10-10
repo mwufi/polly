10147d4ca63e05b5cd1aed84a2a6d44da83429bc
zen.tang <zen.tang@bytedance.com>
2023-10-10 17:13:41 -0400

    add a page

---
+++ app/special/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/Button";
+
+export default function Home() {
+  return (
+    <div className="flex gap-2 p-10">
+      <p>special page</p>
+      <Button></Button>
+    </div>
+  )
+}
---
+++ components/Button.tsx
@@ -0,0 +1,5 @@
+export default function Button(){
+  return (
+    <button className="p-2 px-4 bg-blue-100 rounded shadow">hi where</button>
+  )
+}
